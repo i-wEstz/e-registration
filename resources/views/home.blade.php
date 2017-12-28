e545c485fecc439a9fd11f3399cc0d9e256afbda
Choktananan <k.choktananan@accenture.com>
2017-12-28 17:16:27 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @include('includes.head')
+  @include('includes.head')
 </head>
 <body>
-    
-
-    @include('includes.script')
+        <div class="views">
+                <div class="view view-main">
+        <div data-page="login" class="page no-navbar">
+                <!-- Begin: Page Content -->
+              <div class="page-content">
+                    <div class="content-block">
+                        <img class="logo" src="assets/custom/img/logo.svg" alt="Nectar" />
+                    </div>
+            
+                    <h1 class="text-center">Let's Celebrate STM 30th Anniversary</h1>
+                    <div class="content-block">
+                    <div class="buttons-row row">
+                    <span class="fa-2x fa-stack text-center" title="Pin">
+                         <!--<span class="fa fa-circle fa-stack-2x color-youtube"></span>-->
+                         <span class="fa fa-map-marker fa-stack-1x fa-inverse" style="color:red;"></span>
+                     </span>
+                     <h2 class="text-center">สวนนงนุช exhibition hall | 11 กุมภาพันธ์ 2561<h2>
+        
+                     </div>
+                    </div>
+                    
+                     <div class="content-block row">
+                         <div class="col-100">
+                            <p>
+                                    <button class="button button-big button-block 
+                                    button-fill button-raised button-round button-social button-vk"
+                                    style="height:148px;text-align:center;font-size:3em;line-height:148px;">
+                                            <i class="fa fa-angle-double-right" style="font-size:1.5em;line-height:148px;width: 90px;"></i>
+                                            <span>Register</span>
+                                        </button>
+                            </p>
+                        </div>
+                        </div>
+                </div>
+                
+                <!-- End: Page Content -->
+            </div>
+        </div>
+    </div>
+     @include('includes.script')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('index',function () {
     return view('index');
 });
+
+Route::get('home',function () {
+    return view('home');
+});
